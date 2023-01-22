3d2f1cc6db0159c680977300e3915886f818b5bd
mJaJksJ <maksim.m00@mail.ru>
2023-01-22 20:02:11 +0300

    feat: 3-cnf

---
+++ Cnf/Cnf/Cnf.cs
@@ -0,0 +1,116 @@
+﻿namespace Cnf
+{
+    internal static class Cnf
+    {
+        public static string AsString(this bool e)
+        {
+            return e ? "1" : "0";
+        }
+
+        public static bool[] X1 => new bool[] { false, false, false, false, true, true, true, true };
+        public static bool[] X2 => new bool[] { false, false, true, true, false, false, true, true };
+        public static bool[] X3 => new bool[] { false, true, false, true, false, true, false, true };
+        public static bool[] X4
+        {
+            get
+            {
+                var x = new bool[8];
+                for (var i = 0; i < 8; i++)
+                {
+                    x[i] = !X3[i];
+                }
+                return x;
+            }
+        }
+        public static bool[] X5
+        {
+            get
+            {
+                var x = new bool[8];
+                for (var i = 0; i < 8; i++)
+                {
+                    x[i] = X1[i] || X2[i];
+                }
+                return x;
+            }
+        }
+        public static bool[] X6
+        {
+            get
+            {
+                var x = new bool[8];
+                for (var i = 0; i < 8; i++)
+                {
+                    x[i] = !X4[i];
+                }
+                return x;
+            }
+        }
+        public static bool[] X7
+        {
+            get
+            {
+                var x = new bool[8];
+                for (var i = 0; i < 8; i++)
+                {
+                    x[i] = X1[i] && X2[i] && X4[i];
+                }
+                return x;
+            }
+        }
+        public static bool[] X8
+        {
+            get
+            {
+                var x = new bool[8];
+                for (var i = 0; i < 8; i++)
+                {
+                    x[i] = X5[i] || X6[i];
+                }
+                return x;
+            }
+        }
+        public static bool[] X9
+        {
+            get
+            {
+                var x = new bool[8];
+                for (var i = 0; i < 8; i++)
+                {
+                    x[i] = X6[i] || X7[i];
+                }
+                return x;
+            }
+        }
+        public static bool[] X10
+        {
+            get
+            {
+                var x = new bool[8];
+                for (var i = 0; i < 8; i++)
+                {
+                    x[i] = X8[i] && X9[i] && X7[i];
+                }
+                return x;
+            }
+        }
+        public static bool[] F
+        {
+            get
+            {
+                var x = new bool[8];
+                for (var i = 0; i < 8; i++)
+                {
+                    x[i] = X10[i] && (X4[i] == !X3[i])
+                        && (X5[i] == (X1[i] || X2[i]))
+                        && (X6[i] == !X4[i])
+                        && (X7[i] == (X1[i] && X2[i] && X4[i]))
+                        && (X8[i] == (X5[i] || X6[i]))
+                        && (X9[i] == (X6[i] || X7[i]))
+                        && (X10[i] == (X7[i] && X8[i] && X9[i]));
+                }
+                return x;
+            }
+        }
+    }
+}
---
+++ Cnf/Cnf/Program.cs
@@ -0,0 +1,21 @@
+﻿using static Cnf.Cnf;
+
+namespace Cnf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"X{i}\t");
+            }
+            Console.WriteLine($"F\t");
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine($"{X1[i].AsString()}\t{X2[i].AsString()}\t{X3[i].AsString()}\t{X4[i].AsString()}\t{X5[i].AsString()}\t{X6[i].AsString()}\t{X7[i].AsString()}\t{X8[i].AsString()}\t{X9[i].AsString()}\t{X10[i].AsString()}\t{F[i].AsString()}\t");
+            }
+        }
+    }
+}
